9223bfd37e59583c636e96413202b8ce0eebb31d
hudsongl <beckit321@yahoo.com>
2013-09-30 18:16:54 -0400

    Working on Http Request

--- Mobile-Banking/src/com/smithinc/mobile_banking/AccountViewActivity.java
+++ Mobile-Banking/src/com/smithinc/mobile_banking/AccountViewActivity.java
@@ -1,7 +1,24 @@
 package com.smithinc.mobile_banking;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -28,6 +45,57 @@ public class AccountViewActivity extends Activity {
 		
 		
 		
+		
+	}
+	
+	public class grabAccountInfoTask extends AsyncTask<Void, Void, List<String>>
+	{
+
+		@Override
+		protected List<String> doInBackground(Void... params)
+		{
+			StringBuilder stringBuilder;
+//			URL obj = null;
+//			try {
+//				obj = new URL("http://129.252.226.221:8888/user/authenticate");
+//			} catch (MalformedURLException e3) {
+//				// TODO Auto-generated catch block
+//				e3.printStackTrace();
+//			}
+			
+			HttpGet httpGet = new HttpGet("http://129.252.226.221:8888/user/authenticate");
+			
+			HttpClient client = new DefaultHttpClient();
+			HttpResponse response;
+			
+			stringBuilder = new StringBuilder();
+			
+			try
+			{
+				response = client.execute(httpGet);
+				HttpEntity entity = response.getEntity();
+				InputStream stream = entity.getContent();
+				int b;
+				
+				while((b = stream.read()) != -1)
+				{
+					stringBuilder.append((char) b);
+				}
+			} 
+			catch (ClientProtocolException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			return null;
+		}
 		
 	}
 	
